ce9b0e08a5ee4d0e42996d22366222c4efa451f6
68GHz <thome.v2548@gmail.com>
2026-05-17 22:11:30 -0500

    Dashboard added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,23 @@ import { AuthProvider } from './context/auth-context.jsx'
 import Header from './components/Navbar'
 import Home from './pages/Home'
 import AuthLayout from './components/Auth'
+import Dashboard from './pages/Dashboard.jsx'
 import './App.css'
+import DashboardLayout from './layouts/DashboardLayout.jsx'
 
 function App() {
 
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Header />
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<AuthLayout />} />
+          
+          <Route element={<DashboardLayout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
